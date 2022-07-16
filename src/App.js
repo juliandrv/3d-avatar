11cabf940c6d405477aa6ea9cbed42f29f36a8f9
Julián Ramírez <judramirezva@gmail.com>
2022-07-16 16:20:55 -0500

    added model with textures

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
-import Model from './AvatarPose';
+import Model from './Model';
 
 export default function App() {
   return (
